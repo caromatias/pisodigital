48d60f7e175d1c36dbb42e7c426f9a037bbc560a
caromatias <caromatiasm@gmail.com>
2013-11-13 12:06:17 -0300

    ARREGLOS EN MENÚ Y QUIENES SOMOS

--- src/js/global.js
+++ src/js/global.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
+    $('#inicio').click(function(){
+        $.scrollTo('#topPage', 800);
+        $('#inicio').addClass('active');
+        $('#portafolio').removeClass('active');
+        $('#sobre').removeClass('active');
+    });
     $('#sobre').click(function(){
         $.scrollTo('#features', 800);
+        $('#inicio').removeClass('active');
         $('#portafolio').removeClass('active');
         $('#sobre').addClass('active');
     });
     $('#portafolio').click(function(){
         $.scrollTo('#sc-portafolio', 800);
+        $('#inicio').removeClass('active');
         $('#portafolio').addClass('active');
         $('#sobre').removeClass('active');
     });
